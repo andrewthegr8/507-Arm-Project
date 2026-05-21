4babd0432173e7552ddc89bbc91995fc16997bb0
Andrew Jones <ajone168@calpoly.edu>
2026-05-21 09:52:29 -0700

    Added more TMC driver files

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include <sys/_intsup.h>
+#include "TMC429.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */

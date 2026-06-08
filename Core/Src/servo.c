2fc31ed65898f2202ffe62e3feabf57c300d8a5d
Andrew Jones <ajone168@calpoly.edu>
2026-06-08 12:01:29 -0700

    Re did servo timer config

--- Core/Src/servo.c
+++ Core/Src/servo.c
@@ -5,8 +5,9 @@
 
 extern TIM_HandleTypeDef htim3;
 
-#define SERVO_MIN_PULSE 500u   // 0.5ms equivalent in timer counts for 20 ms period (2000*1/50)
-#define SERVO_MAX_PULSE 1000u  // 1.0ms equivalent in timer counts for 20 ms period (2000*1/20)
+#define SERVO_MIDDLE    1500u // 1.5ms equivalent in timer counts for 20 ms period (2000*1/50)
+#define SERVO_MIN_PULSE 1000u  // 1.0ms equivalent in timer counts for 20 ms period (2000*1/20)
+#define SERVO_MAX_PULSE 2000u  // 2.0ms equivalent in timer counts for 20 ms period (2000*1/10)
 
 void Servo_Init(void)
 {
@@ -16,6 +17,11 @@ void Servo_Init(void)
 void Servo_Open(void)
 {
     __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, SERVO_MIN_PULSE);
+}
+
+void Servo_Center(void)
+{
+    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, SERVO_MIDDLE);
 }
 
 void Servo_Close(void)

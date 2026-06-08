0c3e2a2cf6f4dc23f66e7af0c020750bd45a13e9
saramchamness-afk <saramchamness@gmail.com>
2026-06-08 13:36:30 -0700

    synching

--- Core/Src/servo.c
+++ Core/Src/servo.c
@@ -5,9 +5,9 @@
 
 extern TIM_HandleTypeDef htim15;
 
-#define SERVO_MIDDLE    1500u // 1.5ms equivalent in timer counts for 20 ms period (2000*1/50)
-#define SERVO_MIN_PULSE 1000u  // 1.0ms equivalent in timer counts for 20 ms period (2000*1/20)
-#define SERVO_MAX_PULSE 2000u  // 2.0ms equivalent in timer counts for 20 ms period (2000*1/10)
+#define SERVO_MIDDLE    1500u  // 1.5ms equivalent in timer counts for 20 ms period (20000*1.5/20)
+#define SERVO_MIN_PULSE 1000u  // 1.0ms equivalent in timer counts for 20 ms period (20000*1.0/20)
+#define SERVO_MAX_PULSE 2000u  // 2.0ms equivalent in timer counts for 20 ms period (20000*2.0/20)
 
 void Servo_Init(void)
 {
